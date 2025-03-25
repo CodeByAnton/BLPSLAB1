2b1e9bd4e9d73493f3697865225fdb8155341815
mitrian <onlyvideo01@gmail.com>
2025-03-26 00:30:47 +0300

    feat: product crud controller

---
+++ src/main/java/com/blpsteam/blpslab1/controllers/ProductController.java
@@ -0,0 +1,52 @@
+package com.blpsteam.blpslab1.controllers;
+
+import com.blpsteam.blpslab1.dto.ProductRequestDTO;
+import com.blpsteam.blpslab1.dto.ProductResponseDTO;
+import com.blpsteam.blpslab1.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponseDTO getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @GetMapping
+    public Page<ProductResponseDTO> getAllProducts(Pageable pageable) {
+        return productService.getAllProducts(pageable);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
+    public ProductResponseDTO createProduct(@RequestBody ProductRequestDTO productRequestDTO) {
+        return productService.createProduct(productRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ProductResponseDTO updateProduct(@PathVariable Long id, @RequestBody ProductRequestDTO productRequestDTO) {
+        return productService.updateProduct(id, productRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProductById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
