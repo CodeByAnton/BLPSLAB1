960ae1bd9caa59566ec2f816e2ac1f9c744c5864
mitrian <onlyvideo01@gmail.com>
2025-03-25 23:22:52 +0300

    style: semantic annotations for repositories

--- src/main/java/com/blpsteam/blpslab1/repositories/ProductRepository.java
+++ src/main/java/com/blpsteam/blpslab1/repositories/ProductRepository.java
@@ -3,9 +3,11 @@ package com.blpsteam.blpslab1.repositories;
 import com.blpsteam.blpslab1.data.entities.Product;
 import com.blpsteam.blpslab1.data.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByNameAndDescriptionAndSeller(String name, String description, User seller);
     Optional<Product> findByName(String name);
