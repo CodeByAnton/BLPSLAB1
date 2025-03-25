1055039a060f1042a1bf3fe97691ce0a105cf475
mitrian <onlyvideo01@gmail.com>
2025-03-25 20:39:24 +0300

    feat: category entity

---
+++ src/main/java/com/blpsteam/blpslab1/data/entities/Category.java
@@ -0,0 +1,27 @@
+package com.blpsteam.blpslab1.data.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name="category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
