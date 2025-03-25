3b19137fb232d4137f9a0426ca8dbbd5a58ad8d6
mitrian <onlyvideo01@gmail.com>
2025-03-25 21:03:03 +0300

    feat: basic jpa repositories for entities

---
+++ src/main/java/com/blpsteam/blpslab1/repositories/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.blpsteam.blpslab1.repositories;
+
+import com.blpsteam.blpslab1.data.entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
---
+++ src/main/java/com/blpsteam/blpslab1/repositories/CartRepository.java
@@ -0,0 +1,9 @@
+package com.blpsteam.blpslab1.repositories;
+
+import com.blpsteam.blpslab1.data.entities.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
