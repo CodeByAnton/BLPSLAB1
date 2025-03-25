39428e8d0386c5602a2bb96794e1f73fccb0e0bc
mitrian <onlyvideo01@gmail.com>
2025-03-25 20:53:29 +0300

    feat: cartItem entity (it represents one line in the cart)

---
+++ src/main/java/com/blpsteam/blpslab1/data/entities/CartItem.java
@@ -0,0 +1,34 @@
+package com.blpsteam.blpslab1.data.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="cartItem")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private int quantity;
+    @Column(nullable = false)
+    private int unitPrice;
+    @Column(nullable = false)
+    private int totalPrice;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
