a594b26bfe4f7f24e600ac550f88e7726882d58e
mitrian <onlyvideo01@gmail.com>
2025-05-09 15:47:01 +0300

    feat: logging for buyer service

--- src/main/java/com/blpsteam/blpslab1/service/impl/BuyerServiceImpl.java
+++ src/main/java/com/blpsteam/blpslab1/service/impl/BuyerServiceImpl.java
@@ -6,9 +6,12 @@ import com.blpsteam.blpslab1.repositories.secondary.UserRepository;
 import com.blpsteam.blpslab1.service.BuyerService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class BuyerServiceImpl implements BuyerService {
+    private static final Logger log = LoggerFactory.getLogger(BuyerServiceImpl.class);
     private final UserRepository userRepository;
     public BuyerServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -17,6 +20,7 @@ public class BuyerServiceImpl implements BuyerService {
     @Override
     @Transactional
     public void increaseBalance(String username, Long amount) {
+        log.debug("Increasing user balance method");
         if (amount <= 0) {
             throw new IllegalArgumentException("Amount must be greater than zero.");
         }
@@ -25,6 +29,7 @@ public class BuyerServiceImpl implements BuyerService {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         user.setBalance(user.getBalance()+amount);
+        log.info("Increase for user {}, new balance = {}", user.getId(), user.getBalance());
         userRepository.save(user);
     }
 }
