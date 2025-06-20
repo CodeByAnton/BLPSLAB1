a44df5c52696f36d00fb827bcab811097b4a38f5
annton <anton_moldon04@mail.ru>
2025-06-20 17:54:24 +0300

    add: registration pool

--- src/main/java/com/blpsteam/blpslab1/delegators/validateUserRegistrationDelegate.java
+++ src/main/java/com/blpsteam/blpslab1/delegators/validateUserRegistrationDelegate.java
@@ -6,6 +6,7 @@ import com.blpsteam.blpslab1.exceptions.AdminAlreadyExistsException;
 import com.blpsteam.blpslab1.exceptions.UsernameAlreadyExistsException;
 import com.blpsteam.blpslab1.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Component("validateUserRegistrationDelegate")
 @RequiredArgsConstructor
+@Slf4j
 public class validateUserRegistrationDelegate implements JavaDelegate {
     private final UserService userService;
 
     @Override
     public void execute(DelegateExecution execution){
+        log.info("validateUserRegistrationDelegate");
         String username = (String) execution.getVariable("username");
         String password = (String) execution.getVariable("password");
         String roleStr = (String) execution.getVariable("role");
@@ -26,6 +29,7 @@ public class validateUserRegistrationDelegate implements JavaDelegate {
             Role role = Role.valueOf(roleStr);
             userService.register(username, password, role);
         } catch (IllegalArgumentException | AdminAlreadyExistsException | UsernameAlreadyExistsException e) {
+            log.error(e.getMessage());
             throw new BpmnError("invalidArgumentsError");
         }
     }
